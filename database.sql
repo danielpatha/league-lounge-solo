ac5ca62559f54d766f3bc7a205591bd09701ec85
Daniel Pathammavong <patha037@umn.edu>
2023-02-03 21:24:03 -0600

    Add league table SQL text to database.sql

--- database.sql
+++ database.sql
@@ -9,3 +9,17 @@ CREATE TABLE "user" (
     "password" VARCHAR (1000) NOT NULL
 );
 
+CREATE TABLE "league"(
+"id" SERIAL PRIMARY KEY,
+"league_city_name" VARCHAR (80),
+"sport" VARCHAR (80) ,
+"competitive_level" VARCHAR (30),
+"season" VARCHAR (30),
+"day_of_the_week" VARCHAR (100),
+"time_of_day" VARCHAR (30),
+"co_ed" BOOLEAN DEFAULT FALSE,
+"notes" VARCHAR (1000),
+"link" VARCHAR (500),
+"photo" VARCHAR (1000)
+);
+
